5810fe96edd38c46aab719e7163397f52e5401de
Ben Wibking <ben@wibking.com>
2025-11-17 15:41:49 -0500

    add comment to build scripts

--- build_macos.sh
+++ build_macos.sh
@@ -4,5 +4,6 @@ set -x
 cmake -S . -B build -GNinja -DopenPMD_USE_ADIOS2=ON -DopenPMD_USE_HDF5=ON -DopenPMD_USE_MPI=OFF -DVISIT_PLUGIN_DIR="${HOME}/.visit/3.4.2/darwin-arm64/plugins" -DVISIT_PLUGIN_VS_INSTALL_FILE="/Applications/VisIt.app/Contents/Resources/3.4.2/darwin-arm64/include/PluginVsInstall.cmake" $1
 
 cmake --build build
+# the plugin should actually be installed to ~/.visit in the build step
 #cmake --install build
 
