45a55bf90f48aaada154b64f49139c43cdd596f5
Ben Wibking <ben@wibking.com>
2026-05-03 11:47:08 -0400

    fix build for visit 3.5.0

--- build_macos.sh
+++ build_macos.sh
@@ -1,7 +1,7 @@
 ## do a standard build without MPI
 set -x
 
-cmake -S . -B build -GNinja -DopenPMD_USE_ADIOS2=ON -DopenPMD_USE_HDF5=ON -DopenPMD_USE_MPI=OFF -DVISIT_PLUGIN_DIR="${HOME}/.visit/3.4.2/darwin-arm64/plugins" -DVISIT_PLUGIN_VS_INSTALL_FILE="/Applications/VisIt.app/Contents/Resources/3.4.2/darwin-arm64/include/PluginVsInstall.cmake" $1
+cmake -S . -B build -GNinja -DopenPMD_USE_ADIOS2=ON -DopenPMD_USE_HDF5=OFF -DopenPMD_USE_MPI=OFF -DVISIT_PLUGIN_DIR="${HOME}/.visit/3.5.0/darwin-arm64/plugins" -DVISIT_PLUGIN_VS_INSTALL_FILE="/Applications/VisIt.app/Contents/Resources/3.5.0/darwin-arm64/include/PluginVsInstall.cmake" $1
 
 cmake --build build
 # the plugin should actually be installed to ~/.visit in the build step
